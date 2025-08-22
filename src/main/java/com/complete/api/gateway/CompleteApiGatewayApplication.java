7005572208934bdae4a439d178acc99e65e63c54
Isaiah Omame <isaiah@swipe.ng>
2025-08-22 13:22:07 +0100

    Initial commit

---
+++ src/main/java/com/complete/api/gateway/CompleteApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.complete.api.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompleteApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompleteApiGatewayApplication.class, args);
+	}
+
+}
